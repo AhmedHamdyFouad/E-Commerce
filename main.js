b187d652723d81b9b414d38b2ae71ddf3d46d004
Ahmed Hamdy <161432682+AhmedHamdyFouad@users.noreply.github.com>
2024-04-28 10:04:42 +0300

    My Commerce App
    The First Project With Me

---
+++ main.js
@@ -0,0 +1,31 @@
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close");
+// const navlink = document.querySelector("#navbar li a")
+var mainImg = document.getElementById("MainImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+// arraynav = Array.from(navlink)
+
+bar.onclick = () => {
+  nav.classList.add("active");
+};
+close.onclick = () => {
+  nav.classList.remove("active");
+};
+
+smallImg[0].onclick = ()=> {
+  mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = ()=> {
+  mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = ()=> {
+  mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = ()=> {
+  mainImg.src = smallImg[3].src;
+}
+
+
+
